4ed2c035be723e7b4de03ebdf30b7c3ce93f9c2f
zyx3998970 <zyx3998970@gmail.com>
2015-05-31 11:51:29 +0000

    add 404 template, extract base template and my own RequestHandler

--- main.py
+++ main.py
@@ -2,24 +2,36 @@ import os
 import re
 import tornado.httpserver
 from tornado.ioloop import IOLoop
-from tornado.web import RequestHandler, Application, url
+from tornado.web import (RequestHandler as OriginalRequestHandler,
+    Application, url)
 
 location = lambda x: os.path.join(
     os.path.dirname(os.path.realpath(__file__)), x)
 
+
 def is_mobile(request):
-	ua = request.headers.get('User-Agent')
-    return is_mobile = bool(re.search(r'android|ipad|iphone', ua, re.I))
+    ua = request.headers.get('User-Agent')
+    return bool(re.search(r'android|ipad|iphone', ua, re.I))
+
+class RequestHandler(OriginalRequestHandler):
+    """Make some hack."""
+    def get_template_namespace(self):
+        namespace = super(RequestHandler, self).get_template_namespace()
+        customized_context = dict(is_mobile=is_mobile(self.request))
+        namespace.update(customized_context)
+        return namespace
+
 
 class PageNotFoundHandler(RequestHandler):
-	def get(self):
-		self.set_status(404)
-		self.render('404.html', is_mobile=is_mobile(self.request))
+    def get(self):
+        self.set_status(404)
+        self.render('404.html')
 
 
 class IndexHandler(RequestHandler):
     def get(self):
-        self.render("index.html", is_mobile=is_mobile(self.request))
+        self.render("index.html")
+
 
 def make_app():
     settings = {
